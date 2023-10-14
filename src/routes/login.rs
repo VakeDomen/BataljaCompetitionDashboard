091fda4754d07a6858a19cec79c93cf14e8e317d
VakeDomen <vake.domen94@gmail.com>
2023-10-14 08:56:15 +0000

    admin login

--- src/routes/login.rs
+++ src/routes/login.rs
@@ -3,6 +3,7 @@ use serde::Deserialize;
 use crate::controllers::ldap::ldap_login;
 use crate::controllers::jwt::encode_jwt;
 use crate::models::student::Student;
+use std::env;
 
 #[derive(Deserialize)]
 pub struct AuthPost {
@@ -18,16 +19,28 @@ pub async fn login(body: web::Json<AuthPost>) -> HttpResponse {
         Some(n) => n,
         None => return HttpResponse::Unauthorized().finish()
     };
+
+    let password = match credentials.password {
+        Some(n) => n,
+        None => return HttpResponse::Unauthorized().finish()
+    };
+
+    // admin special case
+    if username.eq("admin") {
+        let admin_pw = env::var("ADMIN_PASSWORD").expect("ADMIN_PASSWORD must be set");
+        if password == admin_pw {
+            return match encode_jwt("admin".to_string()) {
+                Ok(token) => HttpResponse::Ok().body(token),
+                Err(e) => HttpResponse::InternalServerError().json(e.to_string()),
+            };
+        }
+    }
     
     let student_number: i32 = match username.parse() {
         Ok(num) => num,
         Err(e) => return HttpResponse::Unauthorized().finish()
     };
     
-    let password = match credentials.password {
-        Some(n) => n,
-        None => return HttpResponse::Unauthorized().finish()
-    };
 
     let ldap_dn_option = match ldap_login(student_number, password).await {
         Ok(b) => b,
